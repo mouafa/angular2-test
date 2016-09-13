8bd542ac22905998eac4068716215f498e272c9a
mouafa ahmed <mouafa@xzone.local>
2016-09-13 22:02:56 +0100

    ready for deploy

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -43,12 +43,14 @@ module.exports = {
         loaders: [
           'html'
         ]
-      }
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
+    // new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: conf.path.src('index.html'),
       inject: true
